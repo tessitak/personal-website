6648f9b4404f4b3248bb5714b12b4be5e758a40d
tessitak <79184153+tessitak@users.noreply.github.com>
2026-02-19 13:55:53 -0800

    Update track.js

--- api/track.js
+++ api/track.js
@@ -18,6 +18,7 @@ module.exports = async function handler(req, res) {
           action_source: 'WEB',
           user_data: {
             ip_address: ip,
+            user_agent: req.headers['user-agent'] || '',
           },
         },
       ],
